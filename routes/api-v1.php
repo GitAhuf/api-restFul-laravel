77516b9500b062407715750fe91370a3daff981a
ahuf <recibosalvaro@gmail.com>
2023-04-26 12:42:13 -0500

    Añadiendo usuarios y configurando postman

--- routes/api-v1.php
+++ routes/api-v1.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\RegisterController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,9 +14,7 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
-Route::get('/prueba',function(){
-    return 'Prueba';
-});
+Route::post('register', [RegisterController::class, 'store'])->name('api.v1.register');
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
