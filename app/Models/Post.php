073a1d6389a832320a9e6a320d1b17db9500670d
ahuf <recibosalvaro@gmail.com>
2023-04-30 22:39:38 -0500

    Generando relaciones

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -11,4 +11,25 @@ class Post extends Model
 
     const BORRADOR = 1;
     const PUBLICADO = 2;
+
+    // Relacion unoa muchos inversa
+
+    public function user(){
+        $this->belongsTo(User::class);
+    }
+
+    public function category(){
+        $this->belongsTo(Category::class);
+    }
+
+    // Relacion muchos a muchos
+
+    public function tags(){
+        $this->belongsToMany(Tag::class);
+    }
+
+    // Relacion uno a muchos polimorfica
+    public function images(){
+        return $this->morphMany(Image::class, 'imageable');
+    }
 }
