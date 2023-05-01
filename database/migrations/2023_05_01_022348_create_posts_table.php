9b2a1a8d207a41ff3f40f7c8237f6c923ec87965
ahuf <recibosalvaro@gmail.com>
2023-04-30 22:02:02 -0500

    Maquetar la bbdd crear modelo fisico

---
+++ database/migrations/2023_05_01_022348_create_posts_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use App\Models\Post;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('name');
+            $table->string('slug');
+            $table->text('extract');
+            $table->longText('body');
+            $table->enum('status', [Post::BORRADOR, Post::PUBLICADO])->default(Post::BORRADOR);
+
+            //Sin convenciones de laravel
+            /*
+                $table->unsignedBigInteger('category_id');
+                $table->foreign('category_id')->references('id')->on('categories');
+            */
+            //Con convenciones de laravel
+            $table->foreignId('category_id')->constrained()->onDelete('cascade');
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('posts');
+    }
+};
