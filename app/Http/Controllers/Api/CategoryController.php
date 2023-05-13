60e1a391f2bf6fb63a29b06861d1ed4b22c4ed09
ahuf <recibosalvaro@gmail.com>
2023-05-13 15:17:35 -0500

    filtrando recursos

--- app/Http/Controllers/Api/CategoryController.php
+++ app/Http/Controllers/Api/CategoryController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Category;
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class CategoryController extends Controller
@@ -12,7 +14,8 @@ class CategoryController extends Controller
      */
     public function index()
     {
-        //
+        $categories = Category::all();
+        return $categories;
     }
 
     /**
@@ -20,30 +23,47 @@ class CategoryController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'name' => 'required|max:255',
+            'slug' => 'required|max:255|unique:categories'
+        ]);
+
+        $category = Category::create($request->all());
+
+        return $category;
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show($id)
     {
-        //
+        $category = Category::included()->findOrFail($id);
+        return $category;
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, Category $category)
     {
-        //
+        $request->validate([
+            'name' => 'required|max:255',
+            'slug' => 'required|max:255|unique:categories,slug,' . $category->id,
+        ]);
+
+        $category->update($request->all());
+
+        return $category;
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Category $category)
     {
-        //
+        $category->delete();
+
+        return $category;
     }
 }
--- app/Models/Category.php
+++ app/Models/Category.php
@@ -5,14 +5,37 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+use Illuminate\Database\Eloquent\Builder;
+
 class Category extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['name', 'slug'];
+
+    protected $allowIncluded = ['posts','posts.user'] ;
 
 
     // Relacion uno a muchos
 
     public function posts(){
         return $this->hasMany(Post::class);
+    }
+
+    public function scopeIncluded(Builder $query){
+
+        if(empty($this->allowIncluded) || empty(request('included'))){
+            return;      
+        }
+        $relations = explode(',', request('included'));
+        $allowIncluded = collect($this->allowIncluded);
+
+        foreach($relations as$key =>$relationship){
+            if(!$allowIncluded->contains($relationship)){
+                unset($relations[$key]);
+            }
+        }
+
+        $query->with($relations);
     }
 }
